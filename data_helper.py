1f8943ba6ff8cad8cd6e4ff2640909ba327a492b
zhangfazhan <1650011565@qq.com>
2019-10-08 09:57:43 +0800

    update synonym.json

--- data_helper.py
+++ data_helper.py
@@ -11,19 +11,46 @@ def change_cls_data_to_fasttext(raw_data_path, fasttext_path):
                 lis = line.strip().split('\t')
                 f_w.write("__label__{} {}\n".format(lis[0], " ".join(jieba.lcut(lis[1]))))
 
-def make_synonym_dict(text_file_path, json_file_path):
+def make_synonym_dict(text_file_path, text_file_path2, json_file_path):
     synonym_dict = {}
+
+    with open(text_file_path, encoding='utf-8') as f_r:
+        for line in f_r:
+            lis = line.strip().split('        ')
+            if len(lis) > 1:
+                synonym_dict[lis[0]] = lis[1].split('、')
+
+    with open(text_file_path2, encoding='utf-8') as f_r:
+        for line in f_r:
+            lis = line.strip().split()
+            if lis is not None:
+                if lis[0] not in synonym_dict:
+                    synonym_dict[lis[0]] = [lis[1]]
+                else:
+                    try:
+                        temp = synonym_dict[lis[0]]
+                        if lis[1] not in temp:
+                            synonym_dict[lis[0]] = temp.append(lis[1])
+                    except:
+                        pass
+
+                if lis[1] not in synonym_dict:
+                    synonym_dict[lis[1]] = [lis[0]]
+                else:
+                    try:
+                        temp = synonym_dict[lis[1]]
+                        if lis[0] not in temp:
+                            synonym_dict[lis[1]] = temp.append(lis[0])
+                    except:
+                        pass
+
     with open(json_file_path, 'w', encoding='utf-8') as f_w:
-        with open(text_file_path, encoding='utf-8') as f_r:
-            for line in f_r:
-                lis = line.strip().split('        ')
-                print(lis)
-                if len(lis) > 1:
-                    synonym_dict[lis[0]] = lis[1].split('、')
         json.dump(synonym_dict, f_w, indent=4, ensure_ascii=False)
+
+    print(len(synonym_dict))
 
 
 
 if __name__ == "__main__":
     # change_cls_data_to_fasttext("./test_data/test_classification.txt", "./test_data/test_fasttext_cls.txt")
-    make_synonym_dict('./test_data/synonym.txt', './text_analysis_tools/api/data/synonym.json')
+    make_synonym_dict('./test_data/synonym.txt', './test_data/merge_syno.txt', './text_analysis_tools/api/data/synonym.json')
