75c89f781eca5c47ca09607ac7eebb9cf1e6e948
zhangfazhan <1650011565@qq.com>
2019-10-31 09:59:14 +0800

    add some parameters

--- text_analysis_tools/api/text_classification/fasttext.py
+++ text_analysis_tools/api/text_classification/fasttext.py
@@ -7,13 +7,21 @@ import fasttext.FastText as ff
 
 class FastText:
     def __init__(self, save_model_path=None, train_data_path=None, test_data_path=None, type="train",
-                 k=1, threshold=0.0):
+                 k=1, threshold=0.0, epoch=5, pretrainedVectors="", label="__label__",
+                 lr=0.1, dim=100):
         """
-        fasttext 文本分类
-        :param train_data_path: 训练文本路径
+        fasttext
         :param save_model_path: 模型保存路径
-        :param test_data_path: 测试文本路径， 默认为 None
-        :param type: 运行模式，“train/prediict”
+        :param train_data_path: 训练样本路径
+        :param test_data_path: 测试样本路径
+        :param type: 模式：“train/predict”
+        :param k: 返回结果个数
+        :param threshold: 阈值
+        :param epoch: 训练轮数
+        :param pretrainedVectors: 预训练词向量路径
+        :param label: 标签前缀
+        :param lr: 学习率
+        :param dim: 词向量维度
         """
         self.train_data_path = train_data_path
         self.test_data_path = test_data_path
@@ -21,6 +29,12 @@ class FastText:
         self.type = type
         self.k = k
         self.threshold = threshold
+        self.epoch = epoch
+        self.pretrainedVectors = pretrainedVectors
+        self.label = label
+        self.lr = lr
+        self.dim = dim
+
         if self.type == "predict":
             if not os.path.exists(self.save_model_path):
                 print("MODEL: {} is not EXIST ....")
@@ -31,7 +45,8 @@ class FastText:
             self.classifier = None
 
     def train(self):
-        classifier = ff.train_supervised(self.train_data_path, label_prefix="__label__")
+        classifier = ff.train_supervised(input=self.train_data_path, label="__label__", epoch=self.epoch,
+                                         pretrainedVectors=self.pretrainedVectors, lr=self.lr, dim=self.dim)
         classifier.save_model(self.save_model_path)
         train_result = classifier.test(self.train_data_path)
 
